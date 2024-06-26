5d6562ef3c3a9cbfa7cc326a68bc93fd3afb83af
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-06-26 13:58:07 -0500

    Change logo to NUIT + The Great Deal

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,17 +1,31 @@
 'use client';
-// Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
 
-import { HorizonLogo } from '@/components/icons/Icons';
+// Chakra imports
+import { Flex, Heading, Img, useColorModeValue } from '@chakra-ui/react';
+
 import { HSeparator } from '@/components/separator/Separator';
+import Logo from '../../../../public/img/nuit-lockup.png';
+
+
 
 export function SidebarBrand() {
   //   Chakra color mode
   let logoColor = useColorModeValue('navy.700', 'white');
 
+  // Change logo to white & transparent in dark mode
+  const logoColorFilter = useColorModeValue('', 'brightness(0) invert(1)')
+
   return (
     <Flex alignItems="center" flexDirection="column">
-      <HorizonLogo h="26px" w="146px" my="30px" color={logoColor} />
+      <Img
+        src={Logo.src}
+        pointerEvents="none"
+        userSelect="none"
+        filter={ logoColorFilter }
+      />
+      <Heading size="lg" mb="30px" color={logoColor} pointerEvents="none" userSelect="none">
+        The Real Deal
+      </Heading>
       <HSeparator mb="20px" w="284px" />
     </Flex>
   );
