3eba322a1470c120bf491f9110ce07aa8e2d60c2
Vincent LaGrassa <vincent.lagrassa@northwestern.edu>
2024-08-29 15:42:55 -0500

    Stand-in contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -18,6 +18,8 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
+import { ContactCard } from '@/components/contacts/contactCard'
+
 // React imports
 import { useEffect, useState } from 'react';
 
@@ -32,6 +34,17 @@ const API_URL = process.env.API_URL;
 
 export default function Page() {
 
+  const colorPalette = {
+    "profile": {
+      bg:          useColorModeValue('white',       'purple.800'),
+      text:        useColorModeValue('purple.700',  'white'),
+      icon_bg:     "linear-gradient(15.46deg, #4A25E1 26.3%, #7B5AFF 86.4%)",
+      icon_border: "transparent",
+      icon_text:   "white",
+    }
+  }
+
+
   // -------------- Component(s) --------------
 
   return (
@@ -41,6 +54,25 @@ export default function Page() {
       direction="column"
       position="relative"
     >
+
+      <SimpleGrid columns={{"base": 1, "sm": 1, "md": 2, "lg": 3, "2xl": 4}} gap="32px" px="16px">
+
+        <ContactCard
+          name         = "Dr. Olivia M. Gold"
+          nickname     = "Dr. Gold"
+          description  = "Research scientist in the Gene Lab at Kramer BioGenetics, Inc."
+          colorPalette = {colorPalette.profile}
+        ></ContactCard>
+
+        <ContactCard
+          name         = "Andrew Orlando"
+          nickname     = "Andy"
+          description  = 'Brand Manager for "Clean Machine" at Life Line, Inc.'
+          colorPalette = {colorPalette.profile}
+        ></ContactCard>
+
+      </SimpleGrid>
+
     </Flex>
   );
 }
